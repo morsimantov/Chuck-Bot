930e5f2194c4d0543e2620295f53c8ce967c5394
morsimantov <mor.goodsign@gmail.com>
2024-01-12 15:58:17 +0200

    Move files to src directory

--- index.js
+++
@@ -1,8 +0,0 @@
-import ChuckBot from './chuckBot.js';
-import { config as dotenvConfig } from 'dotenv';
-
-dotenvConfig();
-
-const chuckBot = new ChuckBot(process.env.BOT_TOKEN);
-
-chuckBot.start();
